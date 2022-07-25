5c4952bb2912a6a2775caa38a32cd1860d591063
Eva <mail@gastrodon.io>
2022-07-24 20:14:06 -0400

    Add export file

---
+++ lib.go
@@ -0,0 +1,11 @@
+package std
+
+import (
+	"github.com/gastrodon/psyduck-stdlib/ifunny"
+	"github.com/gastrodon/psyduck-stdlib/psyduck"
+	"github.com/gastrodon/psyduck-stdlib/scyther"
+)
+
+var IFunny = ifunny.Plugin
+var Psyduck = psyduck.Plugin
+var Scyther = scyther.Plugin
