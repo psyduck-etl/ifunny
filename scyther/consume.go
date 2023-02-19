ffa394229dc947f430d57b919dbd138e0a5e1452
Eva <mail@gastrodon.io>
2023-02-19 12:24:56 -0500

    consume queue use done channel
    go get -u

--- scyther/consume.go
+++ scyther/consume.go
@@ -14,9 +14,10 @@ func consumeQueue(parse sdk.Parser, specParse sdk.SpecParser) (sdk.Consumer, err
 		return nil, err
 	}
 
-	return func() (chan []byte, chan error) {
-		data := make(chan []byte, 32)
+	return func() (chan []byte, chan error, chan bool) {
+		data := make(chan []byte)
 		errors := make(chan error)
+		done := make(chan bool)
 
 		next := func(data []byte) (bool, error) {
 			return true, putQueueHead(config, data)
@@ -24,10 +25,12 @@ func consumeQueue(parse sdk.Parser, specParse sdk.SpecParser) (sdk.Consumer, err
 
 		go func() {
 			sdk.ConsumeChunk(next, specParse, data, errors)
+			done <- true
 			close(data)
 			close(errors)
+			close(done)
 		}()
 
-		return data, errors
+		return data, errors, done
 	}, nil
 }
